7865747f938918d07439e076f02e835fc04774a8
noxicaea <122637244+noxicaea@users.noreply.github.com>
2023-02-09 03:22:49 +0800

    Add files via upload

--- 企业员工管理程序.py
+++ 企业员工管理程序.py
@@ -137,29 +137,62 @@ def main():
                 if Employees[uid].grade + update > Employee.Max_grade:
                     Teamleaders[uid] = Teamleader(Employees[uid].name,"T",1,Employees[uid].year,Employees[uid].uid)
                     Employees.pop(uid)
+                    print(f"升级成功！升级后的职位为{Teamleaders[uid].job+str(Teamleaders[uid].grade)}，工资为:{Teamleaders[uid].salary()}")
                 else:
                     Employees[uid].grade += update
+                    print(f"升级成功！升级后的职位为{Employees[uid].job+str(Employees[uid].grade)}，工资为:{Employees[uid].salary()}")
             elif uid in Teamleaders.keys():
                 print(f"{Teamleaders[uid].name},工号:{Teamleaders[uid].uid},当前职位:{Teamleaders[uid].job+str(Teamleaders[uid].grade)},工龄:{Teamleaders[uid].year},工资:{Teamleaders[uid].salary()}")
                 update = int(input("请输入需要增加的级数："))
                 if Teamleaders[uid].grade + update > Teamleader.Max_grade:
                     Managers[uid] = Manager(Teamleaders[uid].name,"M",1,Teamleaders[uid].year,Teamleaders[uid].uid)
                     Teamleaders.pop(uid)
+                    print(f"升级成功！升级后的职位为{Managers[uid].job+str(Managers[uid].grade)}，工资为:{Managers[uid].salary()}")
                 else:
                     Teamleaders[uid].grade += update
+                    print(f"升级成功！升级后的职位为{Teamleaders[uid].job+str(Teamleaders[uid].grade)}，工资为:{Teamleaders[uid].salary()}")
             elif uid in Managers.keys():
                 print(f"{Managers[uid].name},工号:{Managers[uid].uid},当前职位:{Managers[uid].job+str(Managers[uid].grade)},工龄:{Managers[uid].year},工资:{Managers[uid].salary()}")
                 update = int(input("请输入需要增加的级数："))
                 if Managers[uid].grade + update > Manager.Max_grade:
                     Managers[uid].grade = 3
+                    print(f"升级成功！升级后的职位为{Managers[uid].job+str(Managers[uid].grade)}，工资为:{Managers[uid].salary()}")
                 else:
                     Managers[uid].grade += update
+                    print(f"升级成功！升级后的职位为{Managers[uid].job+str(Managers[uid].grade)}，工资为:{Managers[uid].salary()}")
             else:
                 print("工号不存在！")
         
         elif get == "4":
-            pass
-        
+            uid = int(input("请输入工号："))
+            if uid in Employees.keys():
+                print(f"{Employees[uid].name},工号:{Employees[uid].uid},当前职位:{Employees[uid].job+str(Employees[uid].grade)},工龄:{Employees[uid].year},工资:{Employees[uid].salary()}")
+                minus = int(input("请输入需要降级的级数："))
+                if Employees[uid].grade - minus < 1:
+                    Employees[uid].grade = 1
+                    print(f"降级成功！降级后的职位为{Employees[uid].job+str(Employees[uid].grade)}，工资为:{Employees[uid].salary()}")
+                else:
+                    Employees[uid].grade -= minus
+                    print(f"降级成功！降级后的职位为{Employees[uid].job+str(Employees[uid].grade)}，工资为:{Employees[uid].salary()}")
+            elif uid in Teamleaders.keys():
+                print(f"{Teamleaders[uid].name},工号:{Teamleaders[uid].uid},当前职位:{Teamleaders[uid].job+str(Teamleaders[uid].grade)},工龄:{Teamleaders[uid].year},工资:{Teamleaders[uid].salary()}")
+                minus = int(input("请输入需要降级的级数："))
+                if Teamleaders[uid].grade - minus < 1:
+                    Employees[uid] = Employee(Teamleaders[uid].name,"E",10,Teamleaders[uid].year,Teamleaders[uid].uid)
+                    Teamleaders.pop(uid)
+                    print(f"降级成功！降级后的职位为{Employees[uid].job+str(Employees[uid].grade)}，工资为:{Employees[uid].salary()}")
+                else:
+                    Teamleaders[uid].grade -= minus
+                    print(f"降级成功！降级后的职位为{Teamleaders[uid].job+str(Teamleaders[uid].grade)}，工资为:{Teamleaders[uid].salary()}")
+            elif uid in Managers.keys():
+                print(f"{Managers[uid].name},工号:{Managers[uid].uid},当前职位:{Managers[uid].job+str(Managers[uid].grade)},工龄:{Managers[uid].year},工资:{Managers[uid].salary()}")
+                if Managers[uid].grade - minus < 1:
+                    Teamleaders[uid] = Teamleader(Managers[uid].name,"T",6,Managers[uid].year,Managers[uid].uid)
+                    Managers.pop(uid)
+                    print(f"降级成功！降级后的职位为{Teamleaders[uid].job+str(Teamleaders[uid].grade)}，工资为:{Teamleaders[uid].salary()}")
+                else:
+                    Managers[uid].grade -= minus
+                    print(f"降级成功！降级后的职位为{Managers[uid].job+str(Managers[uid].grade)}，工资为:{Managers[uid].salary()}")
         elif get == "5":
             break
         
