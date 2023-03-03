5e32e6f9c02e06f53ebbfb45cb0764ef2e1ed247
noxicaea <122637244+noxicaea@users.noreply.github.com>
2023-03-03 18:12:09 +0800

    Add files via upload

---
+++ EasyChatGPT-API-master/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from markupsafe import Markup
+import openai
+import markdown
+import markdown.extensions.fenced_code
+import markdown.extensions.codehilite
+
+openai.api_key = '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'
+app = Flask(__name__)
+messages = []
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+@app.route('/get_response', methods=['POST'])
+
+def get_bot_response():
+    user_input = request.form['user_input']
+    # print(user_input)
+    messages.append({'role': 'user', 'content': user_input})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    ai_response = completion.choices[0].message['content']
+    # print(ai_response)
+    messages.append({'role': 'assistant', 'content': ai_response})
+    print(messages)
+    return  Markup(markdown.markdown(ai_response, extensions=['fenced_code', 'codehilite']))
+@app.route('/reset')
+def reset():
+    global messages
+    messages = []
+    return "Conversation history has been reset."
+if __name__ == '__main__':
+    app.run(debug=True)
